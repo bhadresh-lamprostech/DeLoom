a7c3a455aff933fb50795fa4376098b1a900e79c
saurabh142001 <saurabhjha1402@gmail.com>
2023-06-07 19:14:42 +0530

    design changes

--- src/pages/RegForm.jsx
+++ src/pages/RegForm.jsx
@@ -2,6 +2,13 @@ import React, { useState } from 'react';
 
 const RegForm = () => {
   const [logoPreview, setLogoPreview] = useState(null);
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    username: '',
+    email: '',
+    logo: null,
+  });
 
   const handleLogoChange = (e) => {
     const file = e.target.files[0];
@@ -11,57 +18,119 @@ const RegForm = () => {
         setLogoPreview(reader.result);
       };
       reader.readAsDataURL(file);
+      setFormData({
+        ...formData,
+        logo: file,
+      });
     }
+  };
+
+  const handleInputChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle form submission here
+    console.log(formData);
   };
 
   return (
     <div className="flex justify-center">
-      <form className="w-full max-w-lg bg-white p-6 rounded-lg shadow-lg">
-        <h2 className="text-2xl font-bold text-gray-800 mb-6">One-Time Registration</h2>
-        <p className="text-gray-600 mb-6">Please provide the following information to complete your one-time registration.</p>
+      <form className="w-full max-w-lg bg-gray-800 p-6 rounded-lg shadow-lg">
+        <h2 className="text-2xl font-bold text-white mb-6">One-Time Registration</h2>
+        <p className="text-gray-300 mb-6">Please provide the following information to complete your one-time registration.</p>
         <div className="flex flex-wrap -mx-3 mb-6">
           <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-first-name">
+            <label className="block uppercase tracking-wide text-gray-300 text-xs font-bold mb-2" htmlFor="grid-first-name">
               First Name
             </label>
-            <input className="appearance-none block w-full bg-gray-100 text-gray-800 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-first-name" type="text" placeholder="Jane" />
+            <input
+              className="appearance-none block w-full bg-gray-700 text-white border border-gray-600 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              id="grid-first-name"
+              type="text"
+              placeholder="Jane"
+              name="firstName"
+              value={formData.firstName}
+              onChange={handleInputChange}
+            />
             <p className="text-red-500 text-xs italic">Please fill out this field.</p>
           </div>
           <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-last-name">
+            <label className="block uppercase tracking-wide text-gray-300 text-xs font-bold mb-2" htmlFor="grid-last-name">
               Last Name
             </label>
-            <input className="appearance-none block w-full bg-gray-100 text-gray-800 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Doe" />
+            <input
+              className="appearance-none block w-full bg-gray-700 text-white border border-gray-600 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              id="grid-last-name"
+              type="text"
+              placeholder="Doe"
+              name="lastName"
+              value={formData.lastName}
+              onChange={handleInputChange}
+            />
           </div>
         </div>
         <div className="flex flex-wrap -mx-3 mb-6">
           <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-username">
+            <label className="block uppercase tracking-wide text-gray-300 text-xs font-bold mb-2" htmlFor="grid-username">
               Username
             </label>
-            <input className="appearance-none block w-full bg-gray-100 text-gray-800 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-username" type="text" placeholder="johndoe" />
+            <input
+              className="appearance-none block w-full bg-gray-700 text-white border border-gray-600 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              id="grid-username"
+              type="text"
+              placeholder="johndoe"
+              name="username"
+              value={formData.username}
+              onChange={handleInputChange}
+            />
             <p className="text-red-500 text-xs italic">Please fill out this field.</p>
           </div>
           <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-email">
+            <label className="block uppercase tracking-wide text-gray-300 text-xs font-bold mb-2" htmlFor="grid-email">
               Email
             </label>
-            <input className="appearance-none block w-full bg-gray-100 text-gray-800 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-email" type="email" placeholder="example@example.com" />
+            <input
+              className="appearance-none block w-full bg-gray-700 text-white border border-gray-600 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              id="grid-email"
+              type="email"
+              placeholder="example@example.com"
+              name="email"
+              value={formData.email}
+              onChange={handleInputChange}
+            />
             <p className="text-red-500 text-xs italic">Please fill out this field.</p>
           </div>
         </div>
         <div className="flex flex-wrap -mx-3 mb-6">
           <div className="w-full px-3">
-            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-logo">
+            <label className="block uppercase tracking-wide text-gray-300 text-xs font-bold mb-2" htmlFor="grid-logo">
               Upload Logo
             </label>
-            <input className="appearance-none block w-full bg-gray-100 text-gray-800 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-logo" type="file" onChange={handleLogoChange} />
-            <p className="text-gray-600 text-xs italic">Accepted formats: JPEG, PNG</p>
+            <input
+              className="appearance-none block w-full bg-gray-700 text-white border border-gray-600 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              id="grid-logo"
+              type="file"
+              accept="image/jpeg, image/png"
+              onChange={handleLogoChange}
+            />
+            <p className="text-gray-300 text-xs italic">Accepted formats: JPEG, PNG</p>
             {logoPreview && (
               <img src={logoPreview} alt="Logo Preview" className="w-32 h-32 mt-4 rounded-lg" />
             )}
           </div>
         </div>
+        <button
+          type="submit"
+          className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+          onClick={handleSubmit}
+        >
+          Submit
+        </button>
       </form>
     </div>
   );
