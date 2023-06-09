6f077124c6f9da4e416eb35f85b811526b53eb5a
purvik6062 <purvikpanchal6336@gmail.com>
2023-06-09 10:32:14 +0530

    pulling alert component

--- src/components/dashboard/MyVideosPage.jsx
+++ src/components/dashboard/MyVideosPage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import VideoCard from "./VideoCard";
+import "../../styles/dashboard/MyVideosPage.css";
 
 function MyVideosPage() {
   const videos = [
@@ -7,51 +8,64 @@ function MyVideosPage() {
       id: 1,
       title: "Video 1",
       description: "Description 1",
-      thumbnail: "https://example.com/thumbnail2.jpg",
+      thumbnail: "src/assets/images/profilePhoto.jpg",
     },
     {
       id: 2,
       title: "Video 2",
       description: "Description 2",
-      thumbnail: "https://example.com/thumbnail2.jpg",
+      thumbnail: "src/assets/images/profilePhoto.jpg",
     },
     {
       id: 3,
       title: "Video 3",
       description: "Description 3",
-      thumbnail: "https://example.com/thumbnail2.jpg",
+      thumbnail: "src/assets/images/profilePhoto.jpg",
     },
     {
       id: 4,
       title: "Video 4",
       description: "Description 4",
-      thumbnail: "https://example.com/thumbnail2.jpg",
+      thumbnail: "src/assets/images/profilePhoto.jpg",
     },
     {
       id: 5,
       title: "Video 5",
       description: "Description 5",
-      thumbnail: "https://example.com/thumbnail2.jpg",
+      thumbnail: "src/assets/images/profilePhoto.jpg",
     },
     {
       id: 6,
       title: "Video 6",
       description: "Description 6",
-      thumbnail: "https://example.com/thumbnail2.jpg",
+      thumbnail: "src/assets/images/profilePhoto.jpg",
     },
     // Add more video objects as needed
   ];
 
   return (
-    <div className="video-page">
-      {videos.map((video) => (
-        <VideoCard
-          key={video.id}
-          title={video.title}
-          description={video.description}
-          thumbnail={video.thumbnail}
-        />
-      ))}
+    // <div className="video-page mt-4">
+    //   {videos.map((video) => (
+    //     <VideoCard
+    //       key={video.id}
+    //       title={video.title}
+    //       description={video.description}
+    //       thumbnail={video.thumbnail}
+    //     />
+    //   ))}
+    // </div>
+
+    <div className="video-page mt-4">
+      <div className="video-page-container">
+        {videos.map((video) => (
+          <VideoCard
+            key={video.id}
+            title={video.title}
+            description={video.description}
+            thumbnail={video.thumbnail}
+          />
+        ))}
+      </div>
     </div>
   );
 }
--- src/components/dashboard/VideoCard.jsx
+++ src/components/dashboard/VideoCard.jsx
@@ -1,26 +1,6 @@
 import React from "react";
 import "../../styles/dashboard/VideoCard.css";
 
-// const VideoCard = ({ title, description }) => {
-//   return (
-//     <div className="video-card">
-//       <div className="video-card-header">
-//         <h3 className="video-card-title">{title}</h3>
-//         <div className="video-card-options">
-//           {/* Three dots options */}
-//           <div className="dots-menu">
-//             <div className="dot"></div>
-//             <div className="dot"></div>
-//             <div className="dot"></div>
-//           </div>
-//         </div>
-//       </div>
-//       <p className="video-card-description">{description}</p>
-//       <button className="view-more-button">View More</button>
-//     </div>
-//   );
-// };
-
 const VideoCard = ({ title, description, thumbnail }) => {
   return (
     <div className="video-card">
